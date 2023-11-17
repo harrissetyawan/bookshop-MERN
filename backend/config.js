effcdfac1d810d59d9a7f25587bc93c9f9add22e
A Haris Setyawan <harrisfm90@gmail.com>
2023-11-17 10:26:16 +0700

    FixJSON

--- backend/config.js
+++ backend/config.js
@@ -1,2 +1,2 @@
 export const PORT = 5555;
-export const mongoDBURL = 'mongodb+srv://root:root@bookstore-mern.sbecpjt.mongodb.net/?retryWrites=true&w=majority';
+export const mongoDBURL = 'mongodb+srv://[UN]:[PW]@bookstore-mern.sbecpjt.mongodb.net/?retryWrites=true&w=majority';
